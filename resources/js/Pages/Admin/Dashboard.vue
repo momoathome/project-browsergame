58368932f4048cc2c745b72a9f7d24c4cc2e7393
Maurice Knoll <mknoll1901@gmail.com>
2025-08-29 13:12:29 +0200

    fix adminpanel

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import { Link, useForm, router } from '@inertiajs/vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
-import ResourceDistribution from '@/Modules/Admin/ResourceDistribution.vue';
 import DashboardUserOverview from '@/Modules/Admin/DashboardUserOverview.vue';
 import DashboardMarket from '@/Modules/Admin/DashboardMarket.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
@@ -45,18 +44,6 @@ function regenerateAsteroids() {
       </h1>
 
       <div class="flex gap-4 w-full">
-<!--         <div class="w-1/3 mb-4">
-          <div class="bg-base rounded-xl w-full border-primary border-4 border-solid">
-            <h2 class="text-xl font-semibold p-4 border-b border-primary bg-base-dark rounded-t-xl text-light">
-              Module
-            </h2>
-            <div class="grid grid-cols-2 gap-4 p-4 text-light">
-              <Link :href="route('admin.progression')" class="font-medium text-lg">Building Progression</Link>
-              <Link :href="route('admin.resources')" class="font-medium text-lg">Resource Management</Link>
-            </div>
-          </div>
-        </div> -->
-
         <div class="w-1/3 mb-4">
           <div class="bg-base rounded-xl w-full border-primary border-4 border-solid">
             <h2 class="text-xl font-semibold p-4 border-b border-primary bg-base-dark rounded-t-xl text-light">

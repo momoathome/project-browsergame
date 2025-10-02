b3c0af0099b0688aac8a805e5ede75f9e287c380
Maurice Knoll <mknoll1901@gmail.com>
2025-10-02 20:48:31 +0200

    fix2 collection

--- orion/Modules/Asteroid/Services/AsteroidGenerator.php
+++ orion/Modules/Asteroid/Services/AsteroidGenerator.php
@@ -2,7 +2,7 @@
 
 namespace Orion\Modules\Asteroid\Services;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Orion\Modules\Asteroid\Models\Asteroid;
